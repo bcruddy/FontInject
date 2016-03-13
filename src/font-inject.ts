c3044fbd755a9d0e8b3bf716f539a493cbf74e11
bcruddy <briancruddy@gmail.com>
2016-03-13 16:26:57 -0400

    code reformat

--- src/font-inject.ts
+++ src/font-inject.ts
@@ -2,16 +2,16 @@
 // brian at ruddy dot io | @bcruddy
 
 class Font {
-    family: string;
-    filename: string;
+    family:string;
+    filename:string;
 }
 
 class FontInject {
 
-    stylesheet: CSSStyleSheet = null;
-    rootPath: string = null;
+    stylesheet:CSSStyleSheet = null;
+    rootPath:string = null;
 
-    constructor(fontDirPath: string) {
+    constructor(fontDirPath:string) {
         if (fontDirPath === void 0) {
             fontDirPath = './';
         } else {
@@ -22,7 +22,7 @@ class FontInject {
         this.rootPath = fontDirPath;
     }
 
-    injectAll(fonts: Font[]): void {
+    injectAll(fonts:Font[]):void {
         if (this.stylesheet === null) {
             this.stylesheet = FontInject.generateStyleSheet();
         }
@@ -30,18 +30,18 @@ class FontInject {
         fonts.forEach(this.insertFont.bind(this));
     }
 
-    insertFont(font: Font): void {
+    insertFont(font:Font):void {
         let rule = this.getFontRule(font);
         this.stylesheet.insertRule(rule, 0);
     }
 
-    getFontRule (font: Font): string {
+    getFontRule(font:Font):string {
         let fontPath = this.rootPath + font.filename;
 
         return '@font-face { src: url(' + fontPath + '); font-family: "' + font.family + '"; }';
     }
 
-    static generateStyleSheet(): CSSStyleSheet {
+    static generateStyleSheet():CSSStyleSheet {
         let style = document.createElement('style');
 
         style.appendChild(document.createTextNode('')); // webkit hack
